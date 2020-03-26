3e31ddf9f3f0190852ae7241db34847fc15f6720
tyler-irving <tirving@basecampcodingacademy.org>
2020-03-26 12:31:30 -0500

    Remote work

---
+++ src/main/java/org/basecampcodingacademy/reflections/domain/Answer.java
@@ -0,0 +1,18 @@
+package org.basecampcodingacademy.reflections.domain;
+
+public class Answer {
+    public Integer id;
+    public Integer responseId;
+    public Integer questionId;
+    public String content;
+
+    public Answer() {}
+
+    public Answer(Integer id, Integer responseId, Integer questionId, String content) {
+        this.id = id;
+        this.responseId = responseId;
+        this.questionId = questionId;
+        this.content = content;
+    }
+}
+
